42406700a09049df97ce11d4e844b06f4b218dd7
davidprager <david_prager@yahoo.com>
2021-12-04 20:34:09 -0500

    Initial commit

---
+++ bomdom.js
@@ -0,0 +1,60 @@
+/*
+ * File: bomdom.js
+ * -----------------
+ * This file contains a variety of code that demonstrates the use of the Browser Object Model (BOM)
+ * and the Document Object Model (DOM).
+ */
+
+"use strict";
+
+/* This function demonstrates some functions of the BOM*/
+function Bomdom() {
+    // window resizing is theoretically possible but disabled by most browsers
+
+
+    //You can open a new window (assuming the browser doesn't stop pop ups)
+
+
+
+    // You can create a variable for the new window and can then close it
+
+
+
+    // alert(), confirm() and prompt() are all window methods.
+
+
+
+    // You can use the location property to access parts of the URL
+
+
+    // You can also use it to go to a different URL
+
+
+
+}
+
+
+
+/* This function displays and changes some attributes in elements
+   It is called by the simplePage.html file */
+function investigate() {
+
+}
+
+/* This function will retrieve a parameter from the URL and display it
+   It is callde by the pageTwo.html file */
+function queryString() {
+
+}
+
+/* This function will demonstrate an animation triggered by a button click
+   It is called by the animation.html file */
+function animate() {
+
+}
+
+/* This function adds an element to an HTML page */
+function createElement() {
+
+}
+
